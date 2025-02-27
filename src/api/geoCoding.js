4b9a40a4a1bf9d527bf9abf74401fb4b18ea77d3
Kalaitechtubee <kalaitechtube2001@gmail.com>
2025-02-27 09:12:00 +0530

    Updated features

--- src/api/geoCoding.js
+++ src/api/geoCoding.js
@@ -57,25 +57,3 @@ export const reverseGeocode = async (lat,lon) => {
     }
 }
 
-// const getPlaces2 = async (text)=>{
-//     const URL = `https://api.openweathermap.org/geo/1.0/direct?q=${text}&limit=7&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`
-//     try {
-//         const res = await fetch(URL);
-//         const data = await res.json();
-
-//         const arr = []
-//         data.map(info=>{
-//             let curr = info.name + ", " + info.state + ", " + info.country
-//             let objData = {
-//                 address:curr,
-//                 lat:info.lat,
-//                 lon:info.lon,
-//             }
-//             arr.push(objData);
-//         })
-//         return arr;
-//     } catch (error) {
-//         console.log(error);
-//     }
-//     return [];
-// }
